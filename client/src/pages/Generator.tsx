ee79c88984ad09e9aa63661db51268bcd9e015fa
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-03 00:13:43 +0530

    new

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Title from "../components/Title";
 import UploadZone from "../components/UploadZone";
 import { RectangleVerticalIcon } from "lucide-react";
+import { RectangleHorizontalIcon } from "lucide-react";
+import { PrimaryButton } from "../components/Buttons";
+
 
 const Generator = () => {
   const [name, setName] = useState("");
@@ -55,7 +58,7 @@ const Generator = () => {
 
           {/* right col */}
           <div className="w-full">
-            <div className="mb-4">
+            <div className="mb-4 text-gray-300">
               <label htmlFor="name" className="block text-sm mb-4">
                 Project Name
               </label>
@@ -106,13 +109,42 @@ const Generator = () => {
             <div className="mb-4 text-gray-300">
               <label className="block text-sm mb-4">Aspect Ratio</label>
               <div className="flex gap-3">
-                <RectangleVerticalIcon onClick={()=>setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer`}/>
+                <RectangleVerticalIcon
+                  onClick={() => setAspectRatio("9:16")}
+                  className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '9:16' ? 'ring-violet-500/50 bg-white/10' : ''}`}
+                />
+                <RectangleHorizontalIcon
+                  onClick={() => setAspectRatio("16:9")}
+                  className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '16:9' ? 'ring-violet-500/50 bg-white/10' : ''}`}
+                />
               </div>
             </div>
 
+            <div className="mb-4 text-gray-300">
+              <label
+                htmlFor="userPrompt"
+                className="block text-sm mb-4"
+              >
+                User Prompt{" "}
+                <span className="text-xs text-violet-400">(optional)</span>
+              </label>
+              <textarea
+                id="userPrompt"
+                rows={4}
+                value={userPrompt}
+                onChange={(e) => setUserPrompt(e.target.value)}
+                placeholder="Describe how you want the narration to be."
+                className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"
+              ></textarea>
+            </div>
 
           </div>
         </div>
+
+        <div className="flex justify-center mt-10">
+          <PrimaryButton />
+        </div>
+
       </form>
     </div>
   );
