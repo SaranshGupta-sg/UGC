56394c2e44a7258d84e64cab7a827d98dc1eb6aa
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-04 21:22:33 +0530

    new

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,16 @@
-import { MenuIcon, XIcon } from "lucide-react";
-import { PrimaryButton } from "./Buttons";
+import { MenuIcon, SparkleIcon, XIcon } from "lucide-react";
+import { GhostButton, PrimaryButton } from "./Buttons";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/react";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn, openSignUp } = useClerk();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const navLinks = [
@@ -41,14 +46,48 @@ export default function Navbar() {
           ))}
         </div>
 
-        <div className="hidden md:flex items-center gap-3">
-          <button className="text-sm font-medium text-gray-300 hover:text-white transition max-sm:hidden">
-            Sign in
-          </button>
-          <PrimaryButton className="max-sm:text-xs hidden sm:inline-block">
-            Get Started
-          </PrimaryButton>
-        </div>
+        {!user ? (
+          // Not logged In
+          <div className="hidden md:flex items-center gap-3">
+            <button
+              onClick={() => openSignIn()}
+              className="text-sm font-medium text-gray-300 hover:text-white transition max-sm:hidden"
+            >
+              Sign in
+            </button>
+            <PrimaryButton
+              onClick={() => openSignUp()}
+              className="max-sm:text-xs hidden sm:inline-block"
+            >
+              Get Started
+            </PrimaryButton>
+          </div>
+        ) : (
+          // Loggged In
+          <div className="flex gap-2">
+            <GhostButton
+              onClick={() => navigate("/plans")}
+              className="border-none text-gray-300 sm:py-1.5"
+            >
+              Credits:
+            </GhostButton>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Generate"
+                  labelIcon={<SparkleIcon size={14} />}
+                  onClick={() => navigate("/generate")}
+                />
+
+                <UserButton.Action
+                  label="Generate"
+                  labelIcon={<SparkleIcon size={14} />}
+                  onClick={() => navigate("/generate")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </div>
+        )}
 
         <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
           <MenuIcon className="size-6" />
