0caad453c2588adcfa5a1b526157c17fdbd39fa1
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-05 15:23:06 +0530

    new

--- server/types/express.d.ts
+++ server/types/express.d.ts
@@ -1,1 +1,11 @@
 import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth: () => { userId: string; has: (permisson: any) => boolean };
+      plan?: string;
+      file: any;
+    }
+  }
+}
