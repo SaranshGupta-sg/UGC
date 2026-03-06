1c0e26ce1f1a7ea4937d80e0ccd2881386910c5b
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-06 21:34:44 +0530

    new

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -1,23 +1,70 @@
 import { Request, Response } from "express";
 import * as Sentry from "@sentry/node";
+import { prisma } from "../configs/prisma.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const createProject = async (req: Request, res: Response) => {
+  let tempProjectId: string;
+  const { userId } = req.auth();
+  let isCreditDeducted = false;
 
-    let tempProjectId: string;
-    const { userId } = req.auth()
-    let isCreditdeducted = false;
+  const {
+    name = " New Project",
+    aspectRatio,
+    userPrompt,
+    productName,
+    productDescription,
+    targetLength = 5,
+  } = req.body;
 
-    const {name= ' New Project', aspectRatio, userPrompt, productName, productDescription, targetLength = 5} = req.body;
+  const images: any = req.files;
 
-    const images: any = req.files;
+  if (images.length < 2 || !productName) {
+    return res.status(400).json({ message: "Please upload at least 2 images" });
+  }
 
-    if (images.length < 2 || !productName) {
-        return res.status(400).json({message: 'Please upload at least 2 images'})
-    }
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
 
-    
+  if (!user || user.credits < 5) {
+    return res.status(401).json({ message: "Insufficient credits" });
+  } else {
+    // deduct credits for image generation
+    await prisma.user
+      .update({
+        where: { id: userId },
+        data: { credits: { decrement: 5 } },
+      })
+      .then(() => {
+        isCreditDeducted = true;
+      });
+  }
 
   try {
+    let uploadedImages = await Promise.all(
+      images.map(async (item: any) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+
+    const project = await prisma.project.create({
+      data: {
+        name,
+        userId,
+        productName,
+        userPrompt,
+        aspectRatio,
+        targetLength: parseInt(targetLength),
+        uploadedImages,
+        isGenerating: true,
+      },
+    });
+
+    tempProjectId = project.id;
   } catch (error: any) {
     Sentry.captureException(error);
     res.status(500).json({ message: error.message });
